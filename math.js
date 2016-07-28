2e7db3e1957581eece9dc9abb9953d14a7b11596
newstatement <yan.b.bastos@gmail.com>
2016-07-28 18:41:14 -0300

    Update math.js

--- math.js
+++ math.js
@@ -1,1 +1,16 @@
- 
+function getVariables(str) {
+	patt = [/\W/,/[0-9]/];
+	var res = [];
+	var i = 0;
+	while (i < str.length) {
+		j = i + 1;
+		while (!patt[0].test(str.substring(i,j)) && 
+		!patt[1].test(str.substring(i,i+1)) && 
+		j <= str.length) {j++};
+		if (j > i+1) {res.push(str.substring(i,j-1))};
+		i = j;
+  }
+  return res;
+}
+
+
